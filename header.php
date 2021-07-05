4cb886bdfe241ba5223655486df72652b788df77
Mr last <51836304+01mrlast@users.noreply.github.com>
2021-07-05 21:27:01 +0200

    1.0.1

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<link href="<?php echo get_stylesheet_directory_uri(); ?>/css/bootstrap-grid.min.css" rel="stylesheet" type="text/css">
 	<?php wp_head(); ?>
 	<style>
 		li {
